cd35afc1e8b8c674780ce341e5502b5ef62ae2e8
davidn10 <david.newhouse@gmail.com>
2023-12-28 10:32:39 -0500

    Update framework_ebp.R

--- R/framework_ebp.R
+++ R/framework_ebp.R
@@ -119,7 +119,8 @@ pop_data[[pop_subdomains]] <- factor(pop_data[[pop_subdomains]],
   pop_subdomains_vec <- NULL 
   if (!is.null(smp_subdomains) && !is.null(pop_subdomains)) {
     pop_subdomains_vec <- pop_data[[pop_subdomains]]
-    smp_subdomains_vec <- smp_data[[smp_subdomains]][smp_subdomains_vec %in% pop_subdomains_vec]
+    smp_subdomains_vec <- smp_data[[smp_subdomains]]
+    smp_subdomains_vec <- smp_subdomains_vec %in% pop_subdomains_vec
   }
   
   
