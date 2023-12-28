4b11649499013e36028b2a0353200d557f104079
davidn10 <david.newhouse@gmail.com>
2023-12-28 18:51:12 -0500

    Update framework_ebp.R

--- R/framework_ebp.R
+++ R/framework_ebp.R
@@ -21,9 +21,15 @@ framework_ebp <- function(fixed, pop_data, pop_domains, pop_subdomains, smp_data
   if (!is.null(weights) && benchmark_weights==weights) {
     smp_vars <- c(as.character(fixed[2]), mod_vars, smp_domains, smp_subdomains, weights,
                   benchmark_level)
+    if !is.null(smp_subdomains) {
+      smp_vars <- c(smp_vars,smp_subdomains)
+    }
   } else {
-    smp_vars <- c(as.character(fixed[2]), mod_vars, smp_domains, smp_subdomains, weights,
+    smp_vars <- c(as.character(fixed[2]), mod_vars, smp_domains, weights,
                   benchmark_level, benchmark_weights)
+    if !is.null(smp_subdomains) {
+      smp_vars <- c(smp_vars,smp_subdomains)
+    }
   }
 
   pop_vars <- c(mod_vars, pop_domains, pop_subdomains, aggregate_to, pop_weights,
