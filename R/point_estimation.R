60a1c5d465af0394b741326a0d942b9fa95d7d51
davidn10 <david.newhouse@gmail.com>
2024-01-20 11:56:54 -0500

    Update point_estimation.R

--- R/point_estimation.R
+++ R/point_estimation.R
@@ -489,8 +489,8 @@ indicators[,"Head_Count"] <- expected_head_count(mu=gen_model$mu,var=var, transf
 
 #Output Ydrmp if selected 
 if (!is.null(Ydump)) {
-  Ydumpdf <- data.frame(framework$pop_domains_vec,indicators[,"Mean"],indicators[,"Head_Count"],gen_model$mu,var,sigma2vu,sigma2eta,model_par$sigmae2est,framework$pop_data[,framework$pop_weights],framework$obs_dom,framework$obs_subdom)
-  colnames(Ydumpdf) <- c("Domain","Mean","Head_Count","Mu","Variance","Var_vu","Var_eta","Var_eps","pop_weight","Observed_dom","Observed_subdom")
+  Ydumpdf <- data.frame(framework$pop_domains_vec,indicators[,"Mean"],indicators[,"Head_Count"],gen_model$mu,var,sigma2vu,sigma2eta,model_par$sigmae2est,framework$pop_data[,framework$pop_weights],framework$obs_dom)
+  colnames(Ydumpdf) <- c("Domain","Mean","Head_Count","Mu","Variance","Var_vu","Var_eta","Var_eps","pop_weight","Observed_dom")
   write.table(Ydumpdf,file=Ydump,row.names = FALSE,append=FALSE,col.names=T, sep=",")
 }
 
