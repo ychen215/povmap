8e25a9db161291b51af7808142c6451cb803489a
David Newhouse <david.newhouse@gmail.com>
2024-04-01 15:32:48 -0400

    Update point_estimation.R

--- R/point_estimation.R
+++ R/point_estimation.R
@@ -514,7 +514,7 @@ if (!is.null(Ydump)) {
 
 
 
-point_estimates <- aggregate_weighted_mean(indicators[,c("Mean","Head_Count")],by=list("Domain" = pop_domains_vec_tmp),w=pop_weights_vec) 
+point_estimates <- aggregate_weighted_mean(indicators),by=list("Domain" = pop_domains_vec_tmp),w=pop_weights_vec) 
 point_estimates <- cbind(point_estimates, data.frame(matrix(ncol=length(framework$indicator_names)-2,nrow=N_dom_pop_tmp)))
 colnames(point_estimates) <- c("Domain",framework$indicator_names)
 return(point_estimates)
